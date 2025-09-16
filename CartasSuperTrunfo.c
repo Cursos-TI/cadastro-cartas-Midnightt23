26ef4184ed29408e941e17a68c49e5244db6c81a
Midnightt23 <jean.passos@nutriz.com.br>
2025-09-16 13:58:51 +0000

    Intermediario

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,6 +2,7 @@
 
 int main (){
 
+
     //declaracao das variaveis referente as cartas
     
     char estado1[1], estado2[2];
@@ -11,6 +12,8 @@ int main (){
     float area_em_km1, area_em_km2;
     float pib1, pib2;
     int pontos_turisticos1, pontos_turisticos2;
+    float densidade_populacional1, densidade_populacional2;
+    float pib_per_capita1, pib_per_capita2;
 
 
     //entrada de dados da primeira carta
@@ -64,6 +67,13 @@ int main (){
     printf ("Digite o numero de pontos turisticos \n");
     scanf ("%d", &pontos_turisticos2);
 
+    //calculo de densidade populacional e pib per capito das cartas 1 e cartas 2
+
+    densidade_populacional1 = populacao1 / area_em_km1;
+    pib_per_capita1 = (pib1 * 1000000000) / populacao1;
+    densidade_populacional2 = populacao2 / area_em_km2;
+    pib_per_capita2 = (pib2 * 1000000000) / populacao2;
+
     //Saida de dados digitados pelo usuario (carta1)
 
     printf ("***Carta1***\n");
@@ -74,6 +84,8 @@ int main (){
     printf ("area_em_km: %.2f \n", area_em_km1);
     printf ("pib: %.2f \n", pib1);
     printf ("pontos_turisticos: %d \n", pontos_turisticos1);
+    printf ("densidade_populacional: %.2f \n", densidade_populacional1);
+    printf ("pib_per_capita: %.2f \n", pib_per_capita1);
 
     //Saida de dados digitados pelo usuario (carta2)
 
@@ -85,6 +97,8 @@ int main (){
     printf ("area_em_km: %.2f \n", area_em_km2);
     printf ("pib: %.2f \n", pib2);
     printf ("pontos_turisticos: %d \n", pontos_turisticos2);
+    printf ("densidade_populacional: %.2f \n", densidade_populacional2);
+    printf ("pib_per_capita: %.2f \n", pib_per_capita2);
 
     return 0;
 
